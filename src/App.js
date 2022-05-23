58a4dcda973af8294f9064b01b0a9b6bb877f31a
Nichole Axel <nfrancis22@gmail.com>
2022-05-23 11:29:00 -0400

    added nav and routing

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import { Layout, Menu } from 'antd';
 import { PlusCircleOutlined, QuestionCircleOutlined, UserOutlined } from '@ant-design/icons';
 import './App.css';
 import Menubar from './components/Menubar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const { Header, Content } = Layout;
 
 function App() {
   return (
-    <Layout className='layout'>
-      <Header>
-       <Menubar />
-      </Header>
-      <Content>
-      <RestaurantList/>
-  </Content>
-  </Layout>
+    <BrowserRouter>
+      <Layout className='layout'>
+        <Header>
+          <Menubar />
+        </Header>
+        <Content>
+          <Routes>
+            <Route path='/random' element={<h1>Random</h1>} />          
+            <Route path='/add' element={<h1>Add Restaurant</h1>} />                   
+            <Route path='/login' element={<h1>Login</h1>} />          
+            <Route path='/' element={<RestaurantList/>} />
+          </Routes>
+        </Content>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
