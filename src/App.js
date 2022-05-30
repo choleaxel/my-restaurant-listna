4c68e171abb5e923ea5742262394f050760a392d
Nichole Axel <nfrancis22@gmail.com>
2022-05-30 13:53:38 -0400

    updated from bc

--- src/App.js
+++ src/App.js
@@ -1,39 +1,96 @@
-
-import { useState, createContext } from 'react'; 
-import RestaurantList from './components/RestaurantList';
-import { Layout, Menu } from 'antd';
-import './App.css';
-import Menubar from './components/Menubar';
+import { useState, createContext } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from 'antd';
+import Menubar from './components/Menubar';
+import RestaurantList from './components/RestaurantList';
 import RestaurantPage from './components/RestaurantPage';
 import Login from './components/Login';
+import AddRestaurant from './components/AddRestaurant';
+import './App.css';
 
 const { Header, Content } = Layout;
 
-export const UserContext = createContext(null)
+export const UserContext = createContext(null);
 
 function App() {
   const [user, setUser] = useState();
   return (
     <BrowserRouter>
-      <UserContext.Provider value={{user, setUser }}> 
-      <Layout className='layout'>
-        <Header>
-          <Menubar user={user}/>
-        </Header>
-        <Content>
-          <Routes>
-            <Route path='/restaurants/:restaurantId' element={<RestaurantPage />} />          
-            <Route path='/random' element={<h1>Random</h1>} />          
-            <Route path='/add' element={!user ? <Login /> : <h1>Add Restaurant</h1>} />                           
-            <Route path='/' element={<RestaurantList/>} />
-          </Routes>
-        </Content>
-      </Layout>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Layout className='layout'>
+          <Header>
+            <Menubar />
+          </Header>
+          <Content>
+            <Routes>
+              <Route path='/restaurants/:restaurantId' element={<RestaurantPage />} />
+              <Route path='/random' element={<h1>Random</h1>} />
+              <Route path='/add' element={
+                !user 
+                  ? <Login />
+                  : <AddRestaurant />
+              } />
+              <Route path='/' element={<RestaurantList />} />
+            </Routes>
+          </Content>
+        </Layout>
       </UserContext.Provider>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState, createContext } from 'react'; 
+// import RestaurantList from './components/RestaurantList';
+// import { Layout, Menu } from 'antd';
+// import './App.css';
+// import Menubar from './components/Menubar';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import RestaurantPage from './components/RestaurantPage';
+// import Login from './components/Login';
+
+// const { Header, Content } = Layout;
+
+// export const UserContext = createContext(null)
+
+// function App() {
+//   const [user, setUser] = useState();
+//   return (
+//     <BrowserRouter>
+//       <UserContext.Provider value={{user, setUser }}> 
+//       <Layout className='layout'>
+//         <Header>
+//           <Menubar user={user}/>
+//         </Header>
+//         <Content>
+//           <Routes>
+//             <Route path='/restaurants/:restaurantId' element={<RestaurantPage />} />          
+//             <Route path='/random' element={<h1>Random</h1>} />          
+//             <Route path='/add' element={!user ? <Login /> : <h1>Add Restaurant</h1>} />                           
+//             <Route path='/' element={<RestaurantList/>} />
+//           </Routes>
+//         </Content>
+//       </Layout>
+//       </UserContext.Provider>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
 
--- src/components/AddRestuarant.jsx
+++ src/components/AddRestuarant.jsx
@@ -3,9 +3,81 @@ import { useNavigate } from "react-router-dom";
 
 
 export default function AddRestaurant() {
+  let navigate = useNavigate();
 
+  const [newRestaurant, setNewRestaurant] = useState({
+    name: '',
+    address: '',
+    cuisine: '',
+    image: '',
+    rating: 5,
+  })
 
+  const [error, setError] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('https://my-first-firestore-bc.web.app/restaurants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newRestaurant),
+    })
+      .then(() => navigate('/'))
+      .catch(setError);
+  }
+
+  const handleChange = (e) => {
+    const newValue = (e.target.name === 'rating') 
+                      ? Number(e.target.value)
+                      : e.target.value
+    setNewRestaurant({
+      ...newRestaurant,
+      [e.target.name]: newValue,
+    });
+  }
+
+  return (
+    <section style={{ margin: '2em 1em' }}>
+      <h1>Add Restaurant</h1>
+      {error && <h2 style={{ color: 'red' }}>{error}</h2>}
+      <form onSubmit={handleSubmit}>
+        <label for="name">
+          Name:
+          <input name="name" type="text" value={newRestaurant.name} 
+            onChange={handleChange} />
+        </label>
+        <br />
+        <label for="address">
+          Address:
+          <input name="address" type="text" value={newRestaurant.address} 
+            onChange={handleChange} />
+        </label>
+        <br />
+        <label for="cuisine">
+          Cuisine:
+          <input name="cuisine" type="text" value={newRestaurant.cuisine} 
+            onChange={handleChange} />
+        </label>
+        <br />
+        <label for="image">
+          Photo:
+          <input name="image" type="url" value={newRestaurant.image} 
+            onChange={handleChange} />
+        </label>
+        <br />
+        <label for="rating">
+          Rating:
+          <input name="rating" type="number" max={5} min={1}
+            step={0.1} value={newRestaurant.rating} 
+            onChange={handleChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  )
 }
 
 
